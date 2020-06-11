0770167ff86cbca9eba20cfa64e628821e6a9c24
khanrahil9759 <44775191+khanrahil9759@users.noreply.github.com>
2020-06-11 08:51:52 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Homepage from "./pages/homepage/Homepage";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Shop from "./pages/shop/Shop";
-
-const HatsPage = () => {
-  return <h1>Hats Page</h1>;
-};
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={Homepage} />
-      <Route path="/shop" exact component={Shop} />
-    </Switch>
+    <Fragment>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/shop" exact component={Shop} />
+      </Switch>
+    </Fragment>
   );
 }
 
